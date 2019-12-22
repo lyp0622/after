82c62608cc270af0c10ff629d8497942f162b2a4
wml1998 <18764598283@163.com>
2019-12-22 20:34:03 +0800

    team-wang--twice

---
+++ src/router/modules/testmsg.js
@@ -0,0 +1,32 @@
+import Layout from '@/layout'
+const testRouter = {
+  path: '/testmsg',
+  component: Layout,
+  redirect: 'noRedirect',
+  name: 'testmsg',
+  meta: {
+    title: '考试管理',
+    icon: 'form'
+  },
+  children: [
+    {
+      path: '/addtest',
+      component: () => import('@/views/test/addtest'),
+      name: 'Addtest',
+      meta: {
+        title: '添加考试',
+        noCache: true
+      }
+    },
+    {
+      path: '/testlist',
+      component: () => import('@/views/test/testlist'),
+      name: 'Testlist',
+      meta: {
+        title: '考试列表',
+        noCache: true
+      }
+    }
+  ]
+}
+export default testRouter
