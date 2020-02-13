d55fc7a8d9084f7aa9b7f0aaee8c856623a53801
lmy <1844006904@qq.com>
2020-02-13 13:51:01 +0800

    213aft

---
+++ txxg/src/components/Tip.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Tip = ()=>{
+    
+    return <div>
+        <h3>新型肺炎预防须知</h3>
+        <div>
+            <div><img src="https://puui.qpic.cn/vupload/0/1580113954413_67dj6qw9r34.png/0"/>个人清洁</div>
+        </div>
+        
+        <div >经常保持双手清洁，尤其在触摸口、鼻或眼之前。</div>
+    </div>
+}
+export default Tip
