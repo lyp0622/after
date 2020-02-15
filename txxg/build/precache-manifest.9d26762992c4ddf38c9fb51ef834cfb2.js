71761999bb47c4804d5e63c8920efbe6483897af
lyp <1183391880@qq.com>
2020-02-15 12:46:29 +0800

    task

---
+++ txxg/build/precache-manifest.9d26762992c4ddf38c9fb51ef834cfb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15c94bab920375de3ac93af22683f04",
+    "url": "/1704B/liuyupeng/txxg/index.html"
+  },
+  {
+    "revision": "b00e96ae8c756a749d57",
+    "url": "/1704B/liuyupeng/txxg/static/css/main.7688ac38.chunk.css"
+  },
+  {
+    "revision": "616fa13dcedd7aa838fd",
+    "url": "/1704B/liuyupeng/txxg/static/js/2.0137b206.chunk.js"
+  },
+  {
+    "revision": "0072bd6cef35e05efb19e508ebcb3012",
+    "url": "/1704B/liuyupeng/txxg/static/js/2.0137b206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00e96ae8c756a749d57",
+    "url": "/1704B/liuyupeng/txxg/static/js/main.0d5c679e.chunk.js"
+  },
+  {
+    "revision": "bd6017b1618ee6cdd5c2",
+    "url": "/1704B/liuyupeng/txxg/static/js/runtime-main.cb255370.js"
+  }
+]);
