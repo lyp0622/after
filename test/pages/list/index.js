4e77f0372b3ca92ddd1941f142d5163815a6f43d
wml1998 <18764598283@163.com>
2020-02-17 15:27:31 +0800

    2.17测试

---
+++ test/pages/list/index.js
@@ -0,0 +1,76 @@
+// pages/list/index.js
+
+const app=getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      btns:["目标","习惯","笔记"],
+      cons:["这是我的目标","这是我的习惯","这是我的笔记"],
+      active:0
+  },
+  toggle:function(e){
+this.setData({
+  active:e.currentTarget.dataset.index
+})
+
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
