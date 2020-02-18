874cffee226d56dc5898019031a81e793c65fb74
wml1998 <18764598283@163.com>
2020-02-18 08:40:11 +0800

    2.17地图的完善

--- sign/src/app.tsx
+++ sign/src/app.tsx
@@ -29,7 +29,8 @@ class App extends Component {
     pages: [
       'pages/index/index',
       'pages/map/index',
-      'pages/intviw/index'
+      'pages/sign/add/index',
+      'pages/sign/location/index'
     ],
     window: {
       backgroundTextStyle: 'light',
@@ -46,13 +47,7 @@ class App extends Component {
       async success (res) {
         if (res.code) {
           //发起网络请求
-          // console.log('code...', res.code)
-          // store.dispatch({
-          //   type: 'LOGIN',
-          //   payload: res.code
-          // });
           let response = await login(res.code);
-          console.log('res...', response);
           // 把openid存储到小程序的本地存储
           wx.setStorageSync('openid', response.data.openid);
         } else {
