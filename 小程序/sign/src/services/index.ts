8662c5615602e41437fb99991262d4d409ba002d
1305665289 <1305665289>
2020-02-20 20:31:50 +0800

    添加面试地址

--- 小程序/sign/src/services/index.ts
+++ 小程序/sign/src/services/index.ts
@@ -1,6 +1,11 @@
 import flyio from '../utils/request';
 
 // 登陆接口
-export function login(code): Promise<any>{
-  return flyio.post('/user/code2session', {code});
+export function login(code): Promise<any> {
+  return flyio.post('/user/code2session', { code });
+}
+
+// 添加面试
+export function addSign(params) {
+  return flyio.post('/sign', params);
 }
